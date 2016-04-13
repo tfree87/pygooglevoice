babfc7d46ca61b3cc745b2223b3603905c125c57
tfree87 <tlf11@albion.edu>
2016-04-13 11:26:48 -0400

    Add support for printing a list of text message conversations

--- googlevoice/cli.py
+++ googlevoice/cli.py
@@ -3,6 +3,7 @@
 
 # Global Imports
 import argparse
+import BeautifulSoup
 from pprint import pprint
 
 
@@ -12,19 +13,62 @@ from voice import Voice
 
 def send_message (voice, text=None, phone_number=None):
     ''' Send a text (SMS) message to a phone number'''
-    
-    phone_number = input('Number to send message to: ')
-    text = raw_input('Message text: ')
-    voice = voice
-    voice.send_sms(phone_number, text)
 
-    
+    # Initialize values
+    voice = voice
+    phone_number = phone_number
+    text = text
+
+    # If no values are passed for phone number and text, ask user for input
+    if phone_number == None:
+        phone_number = input('Number to send message to: ')
+    if text == None:
+        text = raw_input('Message text: ')
+
+    try:
+        voice.send_sms(phone_number, text)
+        print('Message successfully transmitted')
+    except:
+        print('Error: message not transmitted')
+        raise
+
 def get_settings(voice):
     ''' List your Google Voice settings'''
-    
+
     pprint(voice.settings)
 
-    
+
+def print_sms(voice) :
+    '''
+    extractsms 
+    extract SMS messages from BeautifulSoup tree of Google Voice SMS HTML.
+    Output is a list of dictionaries, one per message.
+    '''
+
+    voice = voice
+    voice.sms()
+
+    msgitems = []										# accum message items here
+    #	Extract all conversations by searching for a DIV with an ID at top level.
+    tree = BeautifulSoup.BeautifulSoup(voice.sms.html)			# parse HTML into tree
+    conversations = tree.findAll("div",attrs={"id" : True},recursive=False)
+    for conversation in conversations :
+        print('Convsersation' + ' ' + conversation['id'] + ':')
+        #	For each conversation, extract each row, which is one SMS message.
+        rows = conversation.findAll(attrs={"class" : "gc-message-sms-row"})
+        for row in rows :								# for all rows
+            #	For each row, which is one message, extract all the fields.
+            msgitem = {"id" : conversation["id"]}		# tag this message with conversation ID
+            spans = row.findAll("span",attrs={"class" : True}, recursive=False)
+            for span in spans :							# for all spans in row
+                cl = span["class"].replace('gc-message-sms-', '')
+                msgitem[cl] = (" ".join(span.findAll(text=True))).strip()	# put text in dict
+            msgitems.append(msgitem)					# add msg dictionary to list
+        for item in msgitems:
+            string = '\t' + item['from'] + ' ' + item['text']
+            print(string)
+                
+
 def parse_arguments():
     '''Read arguments from the command line'''
 
@@ -32,18 +76,34 @@ def parse_arguments():
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
-    subparsers = parser.add_subparsers(help='Commands')
+    subparsers = parser.add_subparsers(
+        title='Sub Commands',
+        dest='command',
+        help='Commands'
+    )
 
     # SMS command
-    parser_command = subparsers.add_parser('sms', help='Send a text (SMS) message')
-    parser_command.add_argument('-d', '--dial',
-                    help='Phone number to send an SMS message to')
+    parser_command = subparsers.add_parser(
+        'sms',
+        help='Send a text (SMS) message'
+    )
+    parser_command.add_argument(
+        '-d',
+        '--dial',
+        help='Phone number to send an SMS message to'
+    )
     parser_command.add_argument('-t', '--text',
         help='Text message to submit in a text message (SMS)')
 
     # Settings command
-    parser_command = subparsers.add_parser('settings', help='Display Google Voice settings')
-
+    parser_command = subparsers.add_parser(
+        'settings',
+        help='Display Google Voice settings'
+    )
+    parser_command = subparsers.add_parser(
+        'print',
+        help='Print a list of text messages (SMS)'
+    )
     args = parser.parse_args()
     return args
 
@@ -52,13 +112,15 @@ def main():
     args = parse_arguments()
     voice = Voice()
     voice.login()
+
     if args.command == 'sms':
-        send_message(voice)
+        send_message(voice, phone_number=args.dial, text=args.text)
     elif args.command == 'settings':
         get_settings(voice)
+    elif args.command == 'print':
+        print_sms(voice)
 
 
 if __name__ == '__main__':
     ''' Run main() if this file is executed in Python '''
     main()
-    
